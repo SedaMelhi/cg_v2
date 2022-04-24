4606446e3027288051331aeb8e39022735f93d96
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2022-04-24 23:26:55 +0300

    .

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,35 @@ import * as flsFunction from './modules/functions.js';
 
 flsFunction.isWebp();
 
+const mainTitle = document.querySelectorAll('#main-title path')
+console.log(mainTitle)
+for (let i = 0; i < mainTitle.length; i++){
+    
+    console.log(`Letter ${i} is ${mainTitle[i].getTotalLength()}`)
+}
+
+window.addEventListener("load", function() {
+    //window.setTimeout(start, 1000)
+    //function start(){}
+    const body = this.document.querySelector('body')
+    body.classList.remove("no-scroll")
+    body.classList.add("animation")
+    
+    //от 0 до 6666 увелечение 
+    const headerCount = document.querySelector(".header__count")
+    let number = 6666
+    
+    function countAnimation(i){
+        headerCount.innerHTML = `${i}`
+        if(i < number-31)
+            setTimeout(() => countAnimation(i + 31), 0.1)
+        else if(i < number)
+        setTimeout(() => countAnimation(i + 1), 0.1)
+    }
+    setTimeout(() => countAnimation(1), 6000)
+    
+    //убираем экран загрузки
+    document.querySelector(".load").style.display = "none";
+    
+   
+})
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -7,7 +7,8 @@ export function isWebp(){
         webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
     }
     testWebP(function(support){
-        let className = support === true ? 'webp' : 'no-webp';
+        //let className = support === true ? 'webp' : 'no-webp'; 
+        let className = support === true ? 'no-webp' : 'webp';
         document.documentElement.classList.add(className);
     })
 }
